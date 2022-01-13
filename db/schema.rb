8f5ad90cde38cd7a9e55cd1904b569e12b3cec28
Aidan Aannestad <a.aannestad@gmail.com>
2022-01-13 15:09:49 -0600

    event migration + new event button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_205457) do
+ActiveRecord::Schema.define(version: 2022_01_13_205701) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.boolean "all_day"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string "name"
